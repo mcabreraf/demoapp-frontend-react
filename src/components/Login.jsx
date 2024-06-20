d7fa253f07c109e4f812a0f305d7398a776cebe5
Manuel Cabrera <mancabrera22@gmail.com>
2024-06-19 21:13:50 -0400

    updating login file

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
 
 const Login = () => {
   const [username, setUsername] = useState("")
@@ -13,23 +12,52 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const url = "https://aws-flask-app.manuelprojectsinaws.com/login"
+    const url = "http://127.0.0.1:5000/login"
     const options = {
-        username: username,
-        password: password
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
     }
     try {
-      const response = await axios.post(url, options)
-      if (response.status === 200) {
-        localStorage.setItem("token", response.data.access_token)
+      const response = await fetch(url, options)
+      if (response.ok) {
+        const data = await response.json()
+        localStorage.setItem("token", data.access_token)
         navigate("/contacts")
       } else {
-        alert("Invalid credentials. Please try again.", response.data.message)
+        const data = await response.json()
+        alert("Invalid credentials. Please try again.", data.message)
       }
     } catch (error) {
       alert("An error occurred. Please try again later.")
     }
   }
+
+  // # Same function but with Axios (this is for testing purposes)
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
+  //   const url = "http://127.0.0.1:5000/login"
+  //   const options = {
+  //       username: username,
+  //       password: password
+  //   }
+  //   try {
+  //     const response = await axios.post(url, options)
+  //     if (response.status === 200) {
+  //       localStorage.setItem("token", response.data.access_token)
+  //       navigate("/contacts")
+  //     } else {
+  //       alert("Invalid credentials. Please try again.", response.data.message)
+  //     }
+  //   } catch (error) {
+  //     alert("An error occurred. Please try again later.")
+  //   }
+  // }
 
   const checkToken = async (navigate) => {
     const token = localStorage.getItem("token")
